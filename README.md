b3a0a6682da5e37ede8c6e8a9cfdb3d05607dd4a
unknown <atulk9996@gmail.com>
2024-02-19 23:18:17 +0530

    JSX, React.createElement vs JSX, Babel vs parcel in jsx, component, functional component

--- README.md
+++ README.md
@@ -48,3 +48,72 @@
 
 
 https://drive.google.com/file/d/1Ermc-krhwXMt5W3JsqUISyP8PnnZ3C8a/view?usp=drive_link
+
+ep3
+----------------------
+Q. how to create script ? how to run project as per industrial standard?
+ans. create script using package.json - create script to start the application & build the application
+    "scripts": {
+    "start":"parcel index.html",
+    "build":"parcel build index.html"    
+  },
+  cmd to start dev mode app     :   npm run start or npm start
+  cmd to start prod mode app    :   npm run build
+
+- React.createElement ==> object ==> HTMLElement (render)
+- when we ren this element onto the DOM ten it become the HTMLelement
+
+Q. what is jsx?
+ans.
+-   its stand for javascript and xml
+-   its syntex extension for javascript
+-   its systex looks like html or xml
+-   it helps in creating more readabl and concise code(short & accurate)
+-   In jsx - attribute should be in camelCase(eg. className,tabIndex)
+-   JSX( transpile or convert before it reaches the js engine) - parcel - babel
+-   who is transpiling ? ==> parcel but parcel is not doing itself
+                            Babel(package) is converting into code that react/browser understand
+
+# Babel
+- JS compiler (also call transpiler)
+- babel job is to transpiled or convert JSX to js code/react code that understand browser
+JSX ==> Babel ==> convert code that js engine understand
+
+Q. how the jsx works?
+ans. jsx (transpiled or convert into )==> React.createElement (created the ) ==> ReactElement (react element is Js object ) - (render to dom & gives html element ) ==>  HTMLelement (render)
+
+-   In JSX -> wrape multiline of code into () paranthesis bcoz babel need to understand.
+
+-------------------------
+# React component
+1. functional comp
+2. class comp
+
+1. Functional comp
+- function name should in CapitalLetter HeadingComponent
+- functional component is a normal js function which return some piece of code/JSX element and react element
+
+const HeadingComponent = () => {
+    return <h1>Hello JSX</h1>;
+}
+const HeadingComponent = () => {
+    return(
+        <>
+            <h1>Hello JSX</h1>;
+        <>
+    ) 
+}
+Q. how to render functional component inside the root?
+Q. what is component composition ?
+ans. put one component inside the another component or one component  can refers to other component.
+
+- Superpower of JSX { } you can run any js expression or code inside the curly braces
+- u can put react element inside component ---- u can put react element inside other react element 
+
+
+-------------------------
+# vs extension 
+1. pretier code 
+2. bracket pair colorization toggler
+3. ESLint - helpfull for linkdin
+4. better comments 
--- app.js
+++ app.js
@@ -1,80 +1,64 @@
 import React from "react"; 
-import ReactDOM from 'react-dom/client'
-
-    // console.log(React);
-    // console.log(ReactDOM);
-    /*
-     const heading = React.createElement('div',
-     {id:'heading'}, 
-     'hello world react '
-     );
-    */
-    
-     /*
-     type 1
-    <div id='parent'>
-                <div id='child'>
-                    <h1>im h1 tag</h1>    
-                </div>
-            </div>
-     type 2
-            <div id='parent'>
-                <div id='child'>
-                    <h1>im h1 tag</h1>
-                    <h2>im h2 tag</h2>
-                </div>
-            </div>
-     type 3
-
-            <div id='parent'>
-                <div id='child'>
-                    <h1>im h1 tag</h1>
-                </div>
-                <div id='child'>
-                    <h2>im h2 tag</h1>
-                </div>
-            </div>
-     */
-    /*
-    type 1
-     const parent = React.createElement('div', 
-     {id:'parent'}, 
-      React.createElement('div',{id:'child'}, 
-       React.createElement('h1',{},'im h1 tag') 
-      ) 
-     );
-     */
-
-     /* 
-     Type 2
-    const parent = React.createElement('div', 
-     {id:'parent'}, 
-      React.createElement('div',{id:'child'}, 
-      [React.createElement('h1',{},'im h1 tag'),React.createElement('h2',{},'im h2 tag')]
-      ) 
-     );
-     */
-
-     
-    // Type 3
-    const parent = React.createElement('div', 
-     {id:'parent'}, 
-      [
-        React.createElement('div',{id:'child1'}, 
-      [React.createElement('h1',{},'im child1 h1 tag'),React.createElement('h2',{},'im  child1 h2 tag')]
-      ),
-        React.createElement('div',{id:'child2'}, 
-      [React.createElement('h1',{},'im child2 h1 tag'),React.createElement('h2',{},'im child2 h2 tag')]
-      ) 
-         ]
-     );
+import ReactDOM from 'react-dom/client' 
      
     
-    // console.log(heading);
+     // React.createElement ==> ReactElement (JS object) ==> HTMLElemet (render)
+
+    // const parent = React.createElement('div', 
+    //  {id:'parent'}, 
+    //   [
+    //     React.createElement('div',{id:'child1'}, 
+    //   [React.createElement('h1',{},'im child1 h1 tag'),React.createElement('h2',{},'im  child1 h2 tag')]
+    //   ),
+    //     React.createElement('div',{id:'child2'}, 
+    //   [React.createElement('h1',{},'im child2 h1 tag'),React.createElement('h2',{},'im child2 h2 tag')]
+    //   ) 
+    //      ]
+    //  );
+      // console.log(heading);
     // console.log(typeof heading);
+
+     // JSX 
+     // JSX (transpile/convert before it reaches the js engine) - Pacel - Babel
+     // JSX ==> React.createElement ==> ReactElement (JS object) ==> HTMLElemet (render)
+    //  const jsxheading = <h1 id="heading">Hello  JSX </h1>
+    // console.log(jsxheading);
+    
+    // component - functional & class component
+    //component composition
+    // one component put inside the other component that is component composition
+    // u can put react element inside component ---- u can put react element inside other react element 
+    const Title = () => (
+        <h2 className="head" tabIndex="5">
+            React JSX
+        </h2>
+    );
+    const elem = <span>New React element inside the another element -------</span>
+    const subTitle = (
+        <h3>{elem} This React element integrate inside the component  </h3>
+        
+    );
+
+    const number = 10000;
+
+    //functional component
+    const HeadingComponent = () => (
+        <div id="container">
+            {Title()}
+            <Title />
+            <Title></Title>
+            <h3>Inject js code inside curly braces : {number + 10} <br/> 150+100 {150 + 100}
+                <br /> {console.log(typeof Title, typeof HeadingComponent, typeof root)} </h3>
+            <section>
+                {subTitle}    
+            </section>    
+            <h1 className="heading">React functional component</h1>
+        </div>
+    );
+   
 
     const root = ReactDOM.createRoot(document.getElementById('root'));
     // console.log(root);
 
-    root.render(parent)
+    root.render(<HeadingComponent />)
  
